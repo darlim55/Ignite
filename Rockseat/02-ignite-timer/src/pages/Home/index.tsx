698e172d0fe0338747b72538936468906c94de1b
Jorge Darlim <jorge.darlim@oceanbrasil.com>
2023-01-29 10:07:01 -0400

    projeto2

--- Rockseat/02-ignite-timer/src/pages/Home/index.tsx
+++ Rockseat/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -8,16 +12,67 @@ import {
   StartCountDownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
+
+const newcyleformvalidationschema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmonut: zod.number().min(5).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newcyleformvalidationschema>
+
+// guarda informoces do contador
+interface Cycle {
+  id: string
+  task: string
+  minutesAmonut: number
+}
 
 export function Home() {
+  // estado usads para guardar informcoes do contador e
+  // o id do ultimo startado
+
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newcyleformvalidationschema),
+    defaultValues: {
+      task: '',
+      minutesAmonut: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+
+    // pega a data atual em milissgundos
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmonut: data.minutesAmonut,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    reset()
+  }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  console.log(activeCycle)
+  const isSubmitDisable = watch('task')
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             placeholder="Dê um nome para o seu projeto"
             id="task"
+            {...register('task')}
           ></TaskInput>
 
           <label htmlFor="minutesAmonut">durante</label>
@@ -28,6 +83,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmonut', { valueAsNumber: true })}
           ></MinutesAmountInt>
 
           <span>Minutos.</span>
@@ -40,7 +96,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountDownButton disabled type="submit">
+        <StartCountDownButton disabled={!isSubmitDisable} type="submit">
           <Play size={24} />
           Começar
         </StartCountDownButton>
