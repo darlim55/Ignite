a0a6dff050de61ef1c689e30b76caa4fd318fb90
jorge <jorge.silva@grupoicts.com>
2023-08-05 10:41:50 -0400

    modelul01

--- Rockseat/03-DT-Money/src/componentes/Header/index.tsx
+++ Rockseat/03-DT-Money/src/componentes/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logimg from '../../assets/Logo.svg'
 
+import { NewTransactionModal } from "../NewTransactionModal";
+import * as Dialog from "@radix-ui/react-dialog";
 export function Header(){
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logimg} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                    <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                        <NewTransactionModal />
+                    </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
         
