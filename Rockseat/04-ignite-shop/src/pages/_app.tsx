47829184d3ddf886bfd10f961d47555170212409
jorge <jorge.silva@grupoicts.com>
2023-10-19 14:52:37 -0400

    new module

---
+++ Rockseat/04-ignite-shop/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { Container, Header } from '@/styles/pages/app';
+import { globalStyles } from '../styles/global';
+import type { AppProps } from 'next/app';
+import logoImg from '../assets/logo.svg';
+import Image from 'next/image';
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+      <Container>
+        <Header>
+          <Image src={logoImg.src} alt="" width={100} height={100} />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+  );
+}
