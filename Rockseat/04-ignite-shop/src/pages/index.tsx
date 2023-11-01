d5d6dbd4b7c390130dd6151a47a0108fbd776ed5
jorge <jorge.silva@grupoicts.com>
2023-11-01 14:56:10 -0400

    module2: in progress

--- Rockseat/04-ignite-shop/src/pages/index.tsx
+++ Rockseat/04-ignite-shop/src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { HomeContainer, Product } from "@/styles/pages/home"
 import { styled } from "../styles/styles"
 import camiseta1 from "../assets/camisetas/1.png"
+import camiseta2 from "../assets/camisetas/2.png"
+import camisete3 from "../assets/camisetas/3.png"
+
 import Image from "next/image"
 
 export default function Home() {
@@ -13,6 +16,16 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
     </HomeContainer>
   )
 }
