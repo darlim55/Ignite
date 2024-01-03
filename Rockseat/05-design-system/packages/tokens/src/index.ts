60d6882e53f17ef4590f16e4de51a9257362d291
jorge <jorge.silva@grupoicts.com>
2024-01-03 16:32:57 -0400

    feture new

---
+++ Rockseat/05-design-system/packages/tokens/src/index.ts
@@ -0,0 +1,10 @@
+export * from './colors'
+
+export * from './space'
+
+export * from './radi'
+
+export * from './font'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-height'
